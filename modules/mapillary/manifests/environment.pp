95ca4ce3e3acf5e0d7ebaf6a4a4c01576c040ecb
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2014-01-07 11:42:31 -0800

    mapillary: added ios and graphsearch repo

--- modules/mapillary/manifests/environment.pp
+++ modules/mapillary/manifests/environment.pp
@@ -32,7 +32,15 @@ class mapillary::environment {
     source => 'mapillary/mapillary_ios'
   }
 
+  repository { "${boxen::config::srcdir}/mapillary_ios":
+    source => 'mapillary/mapillary_ios'
+  }
+
   repository { "${boxen::config::srcdir}/mapillary_demos":
     source => 'mapillary/mapillary_demos'
+  }
+
+  repository { "${boxen::config::srcdir}/mapillary_graphsearch":
+    source => 'mapillary/mapillary_graphsearch'
   }
 }
